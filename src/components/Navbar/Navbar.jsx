1fab67d51bc29273ad592aec6485663a95bffb53
Gidi Babz <tunex4ind@gmail.com>
2022-08-01 06:06:16 +0100

    navbarl commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
+	return (
+		<nav className="app__navbar">
+			<div className="app__navbar-logo">
+				<img src={images.gericht} alt="app logo" />
+			</div>
+			<ul className="app__navbar-links">
+				<li className="p-opensans">
+					<a href="#home">Home</a>
+				</li>
+				<li className="p-opensans">
+					<a href="#about">About</a>
+				</li>
+				<li className="p-opensans">
+					<a href="#menu">Menu</a>
+				</li>
+				<li className="p-opensans">
+					<a href="#awards">Awards</a>
+				</li>
+				<li className="p-opensans">
+					<a href="#contacts">Contact</a>
+				</li>
+			</ul>
+			<div className="app__navbar-login">
+				<a href="p-opensans">Log In/ Register</a>
+				<div />
+				<a href="p-opensans">Book Table</a>
+			</div>
+
+			{/* Small screen responsiveness */}
+
+			<div className="app__navbar-smallscreen">
+				<GiHamburgerMenu
+					color="#fff"
+					fontSize={27}
+					cursor="pointer"
+					onClick={() => setToggleMenu(true)}
+				/>
+				{toggleMenu && (
+					<div className="app__navbar-smallscreen_overlay flex_center slide-bottom">
+						<MdOutlineRestaurantMenu
+							fontSize={27}
+							className="overlay-close"
+							onClick={() => setToggleMenu(false)}
+						/>
+						<ul className="app__navbar-smallscreen-links">
+							<li className="p-opensans">
+								<a href="#home">Home</a>
+							</li>
+							<li className="p-opensans">
+								<a href="#about">About</a>
+							</li>
+							<li className="p-opensans">
+								<a href="#menu">Menu</a>
+							</li>
+							<li className="p-opensans">
+								<a href="#awards">Awards</a>
+							</li>
+							<li className="p-opensans">
+								<a href="#contacts">Contact</a>
+							</li>
+						</ul>
+					</div>
+				)}
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
